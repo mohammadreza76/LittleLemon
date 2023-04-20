5debcb3f23ae52e8a745038ca5efb5a4ffcc7c14
MohammadReza MoradAli <mohammadrezamoradali1376@yahoo.com>
2023-04-20 15:54:47 +0430

    change db to postgres and make models

--- LittleLemon/LittleLemon/settings.py
+++ LittleLemon/LittleLemon/settings.py
@@ -11,7 +11,7 @@ https://docs.djangoproject.com/en/3.2/ref/settings/
 """
 
 from pathlib import Path
-
+import os
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
@@ -77,13 +77,27 @@ WSGI_APPLICATION = 'LittleLemon.wsgi.application'
 
 # Database
 # https://docs.djangoproject.com/en/3.2/ref/settings/#databases
-
+"""
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
         'NAME': BASE_DIR / 'db.sqlite3',
     }
 }
+"""
+
+
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': 'littlelemon',
+        'USER': 'littleadmin',
+        'PASSWORD': 'littleLemon',
+        'HOST': os.environ.get('DB_HOST'),
+        'PORT': '5432',
+    }
+} 
+
 
 
 # Password validation
