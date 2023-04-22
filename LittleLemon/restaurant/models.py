1ab1f6b69208a1b2c8a0283235a0812f7c34d1cb
MohammadReza MoradAli <mohammadrezamoradali1376@yahoo.com>
2023-04-22 19:37:30 +0430

    remove id field from models code

--- LittleLemon/restaurant/models.py
+++ LittleLemon/restaurant/models.py
@@ -3,7 +3,7 @@ from datetime import datetime
 
 # Create your models here.
 class Booking(models.Model):
-    id = models.IntegerField(primary_key=True)
+ 
     name = models.CharField(max_length=255)
     no_of_guests = models.IntegerField()
     bookingdate = models.DateTimeField()
@@ -11,7 +11,7 @@ class Booking(models.Model):
     
     
 class Menu(models.Model):
-    #id = models.IntegerField(primary_key=True)
+   
     title = models.CharField(max_length=255)
     price = models.DecimalField(max_digits=10,decimal_places=2)
     inventory = models.IntegerField()
